4a338e22bbe57f1de98343b822bc2ea9f0e2be44
“santos05251” <santos05251@outlook.com>
2021-02-23 09:47:17 -0800

    add boading step

--- src/components/account/mealplan-card.jsx
+++ src/components/account/mealplan-card.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const MealIcon = ({ source, notFirst }) => <img src={source} className={`w-12 h-12 rounded-full ${notFirst && "-ml-10"}`} alt="" />;
+const MealIcon = ({ source, notFirst }) => (
+  <img
+    src={source}
+    className={`w-12 h-12 rounded-full ${notFirst && "-ml-10"}`}
+    alt=""
+  />
+);
 
 const MealPlanCard = (props) => {
   let currentDog = {};
@@ -16,11 +22,23 @@ const MealPlanCard = (props) => {
 
   if (currentDog.chicken_recipe) {
     recipeArray.push(cooked_recipes[0].name);
-    iconArray.push(<MealIcon key="chicken_recipe" notFirst={iconArray.length > 0} source={cooked_recipes[0].image_url} />);
+    iconArray.push(
+      <MealIcon
+        key="chicken_recipe"
+        notFirst={iconArray.length > 0}
+        source={cooked_recipes[0].image_url}
+      />
+    );
   }
   if (currentDog.beef_recipe) {
     recipeArray.push(cooked_recipes[1].name);
-    iconArray.push(<MealIcon key="beef_recipe" notFirst={iconArray.length > 0} source={cooked_recipes[1].image_url} />);
+    iconArray.push(
+      <MealIcon
+        key="beef_recipe"
+        notFirst={iconArray.length > 0}
+        source={cooked_recipes[1].image_url}
+      />
+    );
   }
   if (currentDog.turkey_recipe) {
     recipeArray.push(cooked_recipes[2].name);
@@ -34,11 +52,23 @@ const MealPlanCard = (props) => {
   }
   if (currentDog.lamb_recipe) {
     recipeArray.push(cooked_recipes[3].name);
-    iconArray.push(<MealIcon key="lamb_recipe" notFirst={iconArray.length > 0} source={cooked_recipes[3].image_url} />);
+    iconArray.push(
+      <MealIcon
+        key="lamb_recipe"
+        notFirst={iconArray.length > 0}
+        source={cooked_recipes[3].image_url}
+      />
+    );
   }
   if (currentDog.kibble_recipe) {
     recipeArray.push(`${currentDog.kibble_recipe} kibble`);
-    iconArray.push(<MealIcon key="turkey_recipe" notFirst={iconArray.length > 0} source={kibble_recipes[0].image_url} />);
+    iconArray.push(
+      <MealIcon
+        key="turkey_recipe"
+        notFirst={iconArray.length > 0}
+        source={kibble_recipes[0].image_url}
+      />
+    );
   }
 
   let portion = "";
@@ -56,22 +86,18 @@ const MealPlanCard = (props) => {
   let price = subscriptionArray[dogIndex].invoice_estimate_total;
 
   price /= 100;
+
   const readableRecipe = recipeArray.join(" and ");
   return (
-    <div>
-      <p className="pb-8 font-messina text-gray-600 text-15 leading-20">Expected Arrival: March 3rd</p>
-      <div className="w-full font-semibold text-base mb-6 md:flex-col xl:flex-row">
-        <div className="flex">
-          {iconArray}
-          <div className="ml-4 md:ml-4 xl:pr-9">
-            <p className="font-normal text-17 leading-24">{readableRecipe}</p>
-          </div>
-        </div>
-        <div className="flex justify-between mt-6">
-          {" "}
-          <p className="font-normal text-gray-800 pr-4">{portion}</p> {!noPrice && <span className="font-medium">${price}</span>}
+    <div className="w-full flex justify-between font-semibold text-base mb-14 md:flex-col xl:flex-row">
+      <div className="flex">
+        {iconArray}
+        <div className="ml-9 md:ml-4 xl:ml-9">
+          <div>{readableRecipe}</div>
+          <div>{portion}</div>
         </div>
       </div>
+      {!noPrice && <div>${price}</div>}
     </div>
   );
 };
