f6a64c3113812ad11a6ffe907987587fc9a4c44e
“santos05251” <santos05251@outlook.com>
2021-03-06 03:35:57 +0500

    File fixed

--- src/selectors/user.selectors.js
+++ src/selectors/user.selectors.js
@@ -4,11 +4,14 @@ const selectSubscriptionByDogId = (state, dogId) => {
   const subscriptions = selectSubscriptions(state);
   let subscription = null;
 
-  for (const key in subscriptions) {
-  if (subscriptions.hasOwnProperty(key) && subscriptions[key] && +subscriptions[key].dog_id === +dogId) {
-    subscription = subscriptions[key];
-  }
-  }
+  subscriptions.forEach((key, index) => {
+    if (
+      subscriptions[index]
+      && +subscriptions[index].dog_id === +dogId
+    ) {
+      subscription = subscriptions[index];
+    }
+  });
   return subscription;
 };
 
@@ -16,20 +19,20 @@ const selectDogs = (state) => (state.user && state.user.dogs ? state.user.dogs :
 
 const selectDogByIndex = (state, index) => {
   const dogs = selectDogs(state);
-  return dogs[index] || {}
-}
+  return dogs[index] || {};
+};
 
 const selectSubscriptionByDogIndex = (state, index) => {
   const dog = selectDogByIndex(state, index);
   if (dog && dog.id) {
-    return selectSubscriptionByDogId(state, dog.id)
+    return selectSubscriptionByDogId(state, dog.id);
   }
   return null;
 };
 
-const selectUserLoadingByKey = (state, key) => {
-  return state.user.loadingKeys ? state.user.loadingKeys[key] : null;
-}
+const selectUserLoadingByKey = (state, key) => (state.user.loadingKeys
+  ? state.user.loadingKeys[key]
+  : null);
 
 export const userSelectors = {
   selectSubscriptions,
@@ -38,4 +41,4 @@ export const userSelectors = {
   selectDogByIndex,
   selectSubscriptionByDogIndex,
   selectUserLoadingByKey,
-}
+};
